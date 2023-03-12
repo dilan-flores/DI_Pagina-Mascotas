284ac0178a2f521f131918656a8a910185636f75
Dilan <117755180+dilan-flores@users.noreply.github.com>
2023-03-11 19:56:02 -0500

    mejoras móvil y ventas_alimento

---
+++ Proyecto-Mascotas/app_venta_alimento.js
@@ -0,0 +1,36 @@
+let op = document.querySelector("#op")
+op.addEventListener("click", ()=>{
+    op.style.color = "blue";
+});
+
+let op2 = document.querySelector("#op2")
+op2.addEventListener("click", ()=>{
+    op2.style.color = "blue";
+});
+
+let op3 = document.querySelector("#op3")
+op3.addEventListener("click", ()=>{
+    op3.style.color = "blue";
+});
+
+let op4 = document.querySelector("#op4")
+op4.addEventListener("click", ()=>{
+    op4.style.color = "blue";
+});
+
+
+let op5 = document.querySelector("#op5")
+op5.addEventListener("click", ()=>{
+    op5.style.color = "blue";
+});
+
+
+let comprar = document.querySelector(".boton-comprar")
+comprar.addEventListener("click", ()=>{
+    alert("EXITO EN LA COMPRA")
+});
+/*
+function cambiarColor(color){
+    op.style.color = color;
+}
+*/
