c4b59dfe5800168b6ce91648b2717434e77c507f
Dilan <117755180+dilan-flores@users.noreply.github.com>
2023-03-13 16:47:54 -0500

    Mejora de botones y mensajes

--- Proyecto-Mascotas/app_venta_alimento.js
+++ Proyecto-Mascotas/app_venta_alimento.js
@@ -1,34 +1,91 @@
-let op = document.querySelector("#op")
+let op;
+let op2;
+let op3;
+let op4;
+let op5;
+
+let condicion1 = false; // Verifica si está seleccionada alguna opción de "material"
+let condicion2 = false; // Verifica si está seleccionada alguna opción de "tamaño" 
+
+op = document.querySelector("#op")
 op.addEventListener("click", ()=>{
-    op.style.color = "blue";
+    op.style.backgroundColor = "darkcyan";
+    op.style.color = "white";
+
+    op2.style.backgroundColor = "gainsboro";
+    op2.style.color = "black";
+    condicion1 = true;
 });
 
-let op2 = document.querySelector("#op2")
+op2 = document.querySelector("#op2")
 op2.addEventListener("click", ()=>{
-    op2.style.color = "blue";
+    op2.style.backgroundColor = "darkcyan";
+    op2.style.color = "white";
+
+    op.style.backgroundColor = "gainsboro";
+    op.style.color = "black";
+    condicion1 = true;
 });
 
-let op3 = document.querySelector("#op3")
+op3 = document.querySelector("#op3")
 op3.addEventListener("click", ()=>{
-    op3.style.color = "blue";
+    op3.style.backgroundColor = "darkcyan";
+    op3.style.color = "white";
+
+    op4.style.backgroundColor = "gainsboro";
+    op4.style.color = "black";
+
+    op5.style.backgroundColor = "gainsboro";
+    op5.style.color = "black";
+    condicion2 = true;
 });
 
-let op4 = document.querySelector("#op4")
+op4 = document.querySelector("#op4")
 op4.addEventListener("click", ()=>{
-    op4.style.color = "blue";
+    op4.style.backgroundColor = "darkcyan";
+    op4.style.color = "white";
+
+    op3.style.backgroundColor = "gainsboro";
+    op3.style.color = "black";
+
+    op5.style.backgroundColor = "gainsboro";
+    op5.style.color = "black";
+    condicion2 = true;
 });
 
 
-let op5 = document.querySelector("#op5")
+op5 = document.querySelector("#op5")
 op5.addEventListener("click", ()=>{
-    op5.style.color = "blue";
+    op5.style.backgroundColor = "darkcyan";
+    op5.style.color = "white";
+    
+    op3.style.backgroundColor = "gainsboro";
+    op3.style.color = "black";
+
+    op4.style.backgroundColor = "gainsboro";
+    op4.style.color = "black";
+    condicion2 = true;
 });
-
-
+/*
 let comprar = document.querySelector(".boton-comprar")
 comprar.addEventListener("click", ()=>{
     alert("EXITO EN LA COMPRA")
 });
+*/
+function mostrar(){
+    if(condicion1 && condicion2){
+        swal({
+            title:'Compra exitosa',
+            icon:'success'
+        })
+    }else{
+        swal({
+            title:'Compra no realizada',
+            icon:'error'
+        })
+    }
+    
+}
 /*
 function cambiarColor(color){
     op.style.color = color;
